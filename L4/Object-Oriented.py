a3e4be09a1c82fb355314aab985135f1799bc37b
relt <haxrelt@gmail.com>
2022-06-28 18:35:34 +0300

    Использование отдельных модулей для описания классов

--- L4/Object-Oriented.py
+++ L4/Object-Oriented.py
@@ -16,44 +16,7 @@
 # - метод «Изменение цеха» void change_department (int NewDepartment).
 
 import os
-
-class Employee(object):
-    fio : str
-    number : int
-    stage : int
-    age : int
-
-    def __init__(self, fio : str, number : int, stage : int, age : int):
-        self.fio = fio
-        self.number = number
-        self.stage = stage
-        self.age = age
-
-    def grow_up(self, age : int):
-        self.age += age
-
-    def show(self):
-        print('ФИО: ' + self.fio)
-        print('Номер: ' + str(self.number))
-        print('Стаж: ' + str(self.stage))
-        print('Возраст: ' + str(self.age))
-
-class Turner(Employee):
-    experience : int
-    department : int
-
-    def __init__(self, fio : str, number : int, stage : int, age : int, experience : int, department : int):
-        super().__init__(fio, number, stage, age)
-        self.experience = experience
-        self.department = department
-    
-    def change_department(self, department):
-        self.department = department
-    
-    def show(self):
-        super().show()
-        print('Опыт: ' + str(self.experience))
-        print('Отдел: ' + str(self.department))
+from classes import Employee, Turner
 
 employees = []
 turners = []
